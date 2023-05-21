b25fb80b3721028d5d414b71f9c6f6035d4b4efb
fabianosf <32664815+fabianosf@users.noreply.github.com>
2023-05-21 06:24:35 +0000

    Integracao com Banco de Dados

--- app.py
+++ app.py
@@ -1,45 +1,21 @@
 from flask import Flask, render_template, jsonify
+from database import carrega_vagas_db
 
 app = Flask(__name__)
-
-VAGAS = [{
-  'id': 1,
-  'titulo': 'Analista de Dados',
-  'localidade': 'RJ, Brasil',
-  'salario': 'R$ 5.000,00'
-}, {
-  'id': 2,
-  'titulo': 'Desenvolvedor Backend',
-  'localidade': 'PR, Brasil',
-  'salario': 'R$ 4.000,00'
-}, {
-  'id': 3,
-  'titulo': 'Desenvolvedor de Software',
-  'localidade': 'SP, Brasil',
-  'salario': 'R$ 8.500,00'
-}, {
-  'id': 4,
-  'titulo': 'Desenvolvedor Frontend',
-  'localidade': 'RS, Brasil',
-  'salario': 'R$ 6.500,00'
-}, {
-  'id': 5,
-  'titulo': 'Cientista de Dados',
-  'localidade': 'BH, Brasil',
-  'salario': 'R$ 9.500,00'
-}]
 
 
 # Rota Home
 @app.route('/')
 def hello():
-  return render_template('home.html', vagas=VAGAS)
+  vagas = carrega_vagas_db()
+  return render_template('home.html', vagas=vagas)
 
 
 # Endpoint
 @app.route('/vagas')
 def lista_vagas():
-  return jsonify(VAGAS)
+  vagas = carrega_vagas_db()
+  return jsonify(vagas)
 
 
 if __name__ == '__main__':
